1380a5e031146abd27246015a495c56949a9fbc0
Matthew Po <matthewbpo@gmail.com>
2019-11-10 16:24:55 -0500

    re-arranged project structure

--- backend/main.py
+++ backend/main.py
@@ -1,38 +1,4 @@
-# This program will take a video input and extract frames
-import cv2, os
-from singleProcess import singleProcess
-
-def SplitVideo(video_path):
-
-    # Video
-    video = cv2.VideoCapture(video_path)
-
-    # Used to count number of frames
-    frame_count = 0
-
-    # Used to check if fram extraction worked
-    success = True
-
-    while success:
-        # Read from teh video object
-        success, image = video.read()
-
-        # Save the frame with frame count every 20 frames
-        if frame_count % 20 == 0:
-            cv2.imwrite("../images/frame-%d.jpg" % frame_count, image)
-
-        # increment frame_count
-        frame_count += 1
-    print ("...video splitted into images")
-
-# upload images into google cloud storage gs://the-cut-test-bucket
-def uploadImg():
-    os.system("gsutil cp ../images/frame-0.jpg gs://the-cut-test-bucket")
-    print ("...images uploaded")
-    # directory = os.fsencode("../images/")
-    # for file in os.listdir(directory):
-    #     os.system(f"gsutil cp {file} gs://the-cut-test-bucket")
-    # print ('...images uploaded')
+from videoProcess import *
 
 if __name__ == "__main__":
     # Call function
