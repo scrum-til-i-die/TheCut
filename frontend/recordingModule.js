8a49859de320183ca12b77ddaae1962847199287
Stanliest <iamstanley2014@gmail.com>
2020-01-22 22:47:24 -0500

    init recording module

---
+++ frontend/recordingModule.js
@@ -0,0 +1,100 @@
+'use strict';
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+class RecordingModule extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      recording: false,
+      processing: false
+    }
+  }
+
+  async startRecording() {
+    this.setState({ recording: true });
+    // default to mp4 for android as codec is not set
+    const { uri, codec = "mp4" } = await this.camera.recordAsync();
+  }
+
+  stopRecording() {
+    this.camera.stopRecording();
+  }
+
+  render() {
+    const { recording, processing } = this.state;
+
+    let button = (
+      <TouchableOpacity
+        onPress={this.startRecording.bind(this)}
+        style={styles.capture}
+      >
+        <Text style={{ fontSize: 14 }}> RECORD </Text>
+      </TouchableOpacity>
+    );
+
+    if (recording) {
+      button = (
+        <TouchableOpacity
+          onPress={this.stopRecording.bind(this)}
+          style={styles.capture}
+        >
+          <Text style={{ fontSize: 14 }}> STOP </Text>
+        </TouchableOpacity>
+      );
+    }
+
+    if (processing) {
+      button = (
+        <View style={styles.capture}>
+          <ActivityIndicator animating size={18} />
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={"Permission to use camera"}
+          permissionDialogMessage={"We need your permission to use your camera phone"}
+        />
+        <View style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}>
+          {button}
+        </View>
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+export default RecordingModule;
