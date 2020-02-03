e0b3ed04ba8cd85de44e5b8f0639cc10a1344ea0
Stanliest <iamstanley2014@gmail.com>
2020-02-03 17:04:09 -0500

    permission set

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,110 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component, useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
 
-import React, {Component} from 'react';
-import RecordingModule from './recordingModule';
-import Results from './resultsPage';
+class RecordingModule extends Component {
+  state = {
+    video: null,
+    picture: null,
+    recording: false,
+    cameraPermission: false
+  };
 
+  _uploadVideo = async () => {
+    const { video } = this.state;
+    // video.uri
+  };
 
-class App extends Component {
+  _StopRecord = async () => {
+    this.setState({ recording: false }, () => {
+      this.cam.stopRecording();
+    });
+  };
+
+  _StartRecord = async () => {
+    if (this.cam) {
+      this.setState({ recording: true }, async () => {
+        const video = await this.cam.recordAsync();
+        this.setState({ video });
+        console.log(video.uri);
+      });
+    }
+  };
+
+  toogleRecord = () => {
+    const { recording } = this.state;
+
+    if (recording) {
+      this._StopRecord();
+    } else {
+      this._StartRecord();
+    }
+  };
+
+  _showCamera = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status === "granted") {
+      this.setState({ cameraPermission: true });
+    } else {
+      return <Text>No access to camera</Text>;
+    }
+  };
 
   render() {
-    return <RecordingModule />;
+    const { recording, video, cameraPermission } = this.state;
+    console.log("cameraPermission", this.state.cameraPermission);
+    return (
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          flex: 1,
+          width: "100%"
+        }}
+      >
+        {cameraPermission ? (
+          <Camera
+            ref={cam => (this.cam = cam)}
+            style={{
+              justifyContent: "flex-end",
+              alignItems: "center",
+              flex: 1,
+              width: "100%"
+            }}
+          >
+            {video && (
+              <TouchableOpacity
+                onPress={this._uploadVideo}
+                style={{
+                  padding: 20,
+                  width: "100%",
+                  backgroundColor: "#fff"
+                }}
+              >
+                <Text style={{ textAlign: "center" }}>Upload</Text>
+              </TouchableOpacity>
+            )}
+            <TouchableOpacity
+              onPress={this.toogleRecord}
+              style={{
+                padding: 20,
+                width: "100%",
+                backgroundColor: recording ? "#ef4f84" : "#4fef97"
+              }}
+            >
+              <Text style={{ textAlign: "center" }}>
+                {recording ? "Stop" : "Record"}
+              </Text>
+            </TouchableOpacity>
+          </Camera>) : (
+            <TouchableOpacity onPress={this._showCamera}>
+              <Text> Record </Text>
+            </TouchableOpacity>
+          )}
+      </View>
+    );
   }
 }
 
-export default App;
+export default RecordingModule;
