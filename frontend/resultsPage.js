24dfa71ce5866835572ce5b513a6c4eec88a0674
Stanliest <iamstanley2014@gmail.com>
2020-02-25 14:02:44 -0500

    result page design 0

--- frontend/resultsPage.js
+++ frontend/resultsPage.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Container, Header, Content, List, ListItem, Text } from 'native-base';
 const axios = require('axios');
 import { apiKey } from 'react-native-dotenv'
 
 class Results extends Component {
-  state = { 
+  state = {
     movie: 'jurassic world',
-    metadata: [] 
+    metadata: []
   };
 
-  componentWillMount(){
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key='+apiKey+'&language=en-US&query='+this.state.movie+'&page=1')
-      .then(response => 
-        {
-          id = response.data.results[0].id;
-          axios.get('https://api.themoviedb.org/3/movie/'+id+'?api_key='+apiKey)
-          .then(response2 => this.setState({ metadata : response2.data }) )
+  componentWillMount() {
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + apiKey + '&language=en-US&query=' + this.state.movie + '&page=1')
+      .then(response => {
+        id = response.data.results[0].id;
+        axios.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + apiKey)
+          .then(response2 => this.setState({ metadata: response2.data }))
           .catch(function (error) {
             alert(error);
           });
@@ -26,15 +26,54 @@ class Results extends Component {
   }
 
   render() {
-    return(
-      <View style={styles.container}>
-        <Text style={styles.bigBlue}>{this.state.metadata.title}</Text>
-        <Text style={styles.alignCenter}>Overview: {this.state.metadata.overview}</Text>
-        <Text>Budget: ${this.state.metadata.budget}</Text>
-        <Text>Revenue: ${this.state.metadata.revenue}</Text>
-        <Text>Runtime: {this.state.metadata.runtime} min</Text>
-        <Text>Tagline: "{this.state.metadata.tagline}"</Text>
-      </View>
+    return (<Container>
+      <Header />
+      <Content>
+        <List>
+          <ListItem itemDivider>
+            <Text>Movie Title</Text>
+          </ListItem>
+          <ListItem>
+            <Text>{this.state.metadata.title}</Text>
+          </ListItem>
+
+          <ListItem itemDivider>
+            <Text>Overview</Text>
+          </ListItem>
+          <ListItem>
+            <Text>{this.state.metadata.overview}</Text>
+          </ListItem>
+
+          <ListItem itemDivider>
+            <Text>Budget</Text>
+          </ListItem>
+          <ListItem>
+            <Text>${this.state.metadata.budget}</Text>
+          </ListItem>
+
+          <ListItem itemDivider>
+            <Text>Revenue</Text>
+          </ListItem>
+          <ListItem>
+            <Text>${this.state.metadata.revenue}</Text>
+          </ListItem>
+
+          <ListItem itemDivider>
+            <Text>Runtime</Text>
+          </ListItem>
+          <ListItem>
+            <Text>{this.state.metadata.runtime} min</Text>
+          </ListItem>
+
+          <ListItem itemDivider>
+            <Text>Tagline</Text>
+          </ListItem>
+          <ListItem>
+            <Text>{this.state.metadata.tagline}</Text>
+          </ListItem>
+        </List>
+      </Content>
+    </Container>
     )
   }
 };
