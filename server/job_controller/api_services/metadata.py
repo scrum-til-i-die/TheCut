6fb4ecb61e9048918927833481109f7d6cdf0fec
Suhavi Sandhu <beautyspell5@gmail.com>
2020-03-09 22:19:26 -0400

    Metadata api refactor (#51)
    * added error check for if movie exists
    
    * Updated function name

--- server/job_controller/api_services/metadata.py
+++ server/job_controller/api_services/metadata.py
@@ -7,26 +7,19 @@ API_KEY = os.getenv('PUBLIC_KEY')
 
 base_url = "https://api.themoviedb.org/3"
 
-# Returns metadata of movie given title
-def get_metadata(movie_name):
-    response = get_movie_id(movie_name)
-    movie = response.json()["results"][0]
-    return get_metadata_from_id(movie["id"])
-
-# Helper function to get movie id
+# Get movie id, returns -1 if movie does not exist
 def get_movie_id(movie_name):
     params = {
         "api_key": API_KEY,
         "query": movie_name
     }
-    return requests.get(base_url+'/search/movie', params)
+    data = requests.get(base_url+'/search/movie', params).json()
+    # Check if movie is actually a movie
+    return -1 if not data["results"] else data["results"][0]["id"]
 
-# Helper function to get movie metadata
-def get_metadata_from_id(movie_id):
+# Get movie metadata from id
+def get_metadata(movie_id):
     params = {
         "api_key": API_KEY,
     }
-    return requests.get(base_url+'/movie/'+str(movie_id), params)
-
-
-print(get_metadata("inception").json())
+    return requests.get(base_url+'/movie/'+str(movie_id), params).json()
