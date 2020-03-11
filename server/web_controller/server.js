a7baee05d8bd7c4ecf4229ea6003b8b86e43ff77
Matthew Po <matthewbpo@gmail.com>
2020-03-11 14:02:17 -0400

    tied in web and job controllers

--- server/web_controller/server.js
+++ server/web_controller/server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const uuid = require('uuid');
 const fs = require('fs');
+const axios = require('axios');
+const rimraf = require("rimraf");
+const sleep = require('sleep');
 
 const uploadPath = '/app/uploads/'
 var fileName = '';
@@ -32,12 +35,41 @@ app.post('/uploadfile', upload.any(), (req, res) => {
     var fileName = file.originalname;
     var jobId = file.filename.split(".")[0];
 
-    return res.send({
-        status: "File Received",
-        file_name: fileName,
-        job_id: jobId,
-        created_on: Date.now()
+    createJob(jobId).then(function(response){
+        var success = response;
+        sleep.msleep(50);
+        // Dev Error 100: failed to create job.
+        if (success === false){
+            rimraf(`/app/uploads/${jobId}`, function() {});
+            return res.send({
+                status: "Error",
+                message: "Failed to upload video. (Dev Error 100)"
+            })
+        }
+        else{
+            return res.send({
+                status: "File Received",
+                file_name: fileName,
+                job_id: jobId,
+                created_on: Date.now()
+            })
+        } 
     })
+
+    
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
+
+function createJob(jobId){
+    return axios.post('http://localhost:5001/create-job', null, { params: {jobId} })
+    .then(response => {
+        // console.log(response.data);
+        return true;
+    })
+    .catch(response => {
+        // TODO: log error?
+        return false;
+    }
+);
+}
